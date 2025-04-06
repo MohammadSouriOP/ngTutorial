ef79449d4c55dbc30b2cfd44c3d3e8407499f6f6
Mohammad Souri <mohammad.souri@sociumtech.com>
2025-04-06 17:19:41 +0300

    ng

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MasterService } from '../../services/master.service';
+import { APIResponseModel, IDesignation } from '../../model/interface/role';
 
 @Component({
   selector: 'app-designation',
   imports: [],
   templateUrl: './designation.component.html',
-  styleUrl: './designation.component.css'
+  styleUrl: './designation.component.css',
 })
-export class DesignationComponent {
+export class DesignationComponent implements OnInit {
+  designationList: IDesignation[] = [];
+  masterService = inject(MasterService);
 
+  ngOnInit(): void {
+    this.masterService
+      .getDesignations()
+      .subscribe((result: APIResponseModel) => {
+        this.designationList = result.data;
+      });
+  }
 }
